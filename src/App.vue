685131a17a65eff7d42eb3c21664ea6065004cfd
ludovic-mrmt <ludovic.mrmt@eduge.ch>
2017-11-29 10:43:07 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="sideNav">
+    <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
         <v-list-tile>
           <v-list-tile-action>
@@ -11,8 +11,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark class="blue-grey darken-1">
-      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up">
-      </v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon>
       <v-toolbar-title>UltimateTicTacToe</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
