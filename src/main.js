574a0f2fcf2d185d8137824b7aa74b29ba9ab335
ludovic-mrmt <ludovic.mrmt@eduge.ch>
2017-11-29 11:21:56 +0100

    test

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import './stylus/main.styl'
+import router from './router'
 
 Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
