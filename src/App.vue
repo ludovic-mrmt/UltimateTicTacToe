0b0c9ad1389cb80a4b9051c7dfa927e6fe1fb333
ludovic-mrmt <ludovic.mrmt@eduge.ch>
2017-12-06 11:36:20 +0100

    link

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer temporary v-model="sideNav" >
+    <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-tile v-for="item in menuItems" :key="item.title">
+        <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link" @click="">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -11,17 +11,15 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark class="blue-grey darken-1">
-      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer">UltimateTicTacToe</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-         <v-btn flat color="amber darken-3" dark>Le jeu
-           <v-icon right>golf_course</v-icon>
-         </v-btn>
-         <v-btn flat color="green lighten-1" dark>Les règles
-           <v-icon right>attach_file</v-icon>
+         <v-btn flat color="amber darken-3" dark href="https://www.instagram.com/UltimateTicTacToe/">
+           <v-icon left>people</v-icon>
+           Réseaux sociaux
          </v-btn>
       </v-toolbar-items>
     </v-toolbar>
