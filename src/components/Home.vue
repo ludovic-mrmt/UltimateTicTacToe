48fe3dd09643c9ba99092bc969439ae40344689e
ludovic-mrmt <ludovic.mrmt@eduge.ch>
2017-12-09 17:38:27 +0100

    home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="hello">
-    <h1 style="text-align:center;">Bienvenue sur la page de l'UltimateTicTacToe !</h1>
-  </div>
+  <v-container>
+    <v-layout row justify-center wrap>
+      <v-card>
+        <v-card-media src="/static/welcome.png" height="400px">
+        </v-card-media>
+        <v-card-title primary-title>
+          <div>
+            <h1 style="text-align:center;">Bienvenue sur la page de l'UltimateTicTacToe !</h1>
+          </div>
+        </v-card-title>
+      </v-card>
+      <v-flex xs12 class="info">
+        <v-btn large router to="/morpion" class="text-center">Jouer !</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
