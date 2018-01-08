2f525ecbf3a6465c330acf7671ca412481adc6eb
Kelan <kelan.slmn@eduge.ch>
2018-01-08 18:48:33 +0100

    password wtf?????

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -18,13 +18,15 @@
         label="Password"
         v-model="password"
         :counter="10"
+        :rules="passwordRules"
         required
+
       ></v-text-field>
       <v-text-field
         label="Confirm Password"
         v-model="confirmPassword"
-        :rules="[comparePasswords]"
         :counter="10"
+        :rules="CPasswordRules"
 
         required
 
@@ -45,46 +47,33 @@
       return {
         valid: false,
         name: '',
-        nameRules: [
-          (v) => !!v || 'Name is required',
-          (v) => v.length <= 10 || 'Name must be less than 10 characters'
-        ],
         email: '',
-        emailRules: [
-          (v) => !!v || 'E-mail is required',
-          (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-        ],
         Password: '',
-        passwordRules: [
-          (v) => !!v || 'Password is required',
-          (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Password must be valid '
-        ],
-        confirmPassword: '',
-        CPasswordRules: [
-          (v) => !!v || 'Password is required',
-          (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Password must be valid '
-        ]
-
-
+        confirmPassword:''
       }
     },
-    computed: {
-      comparePasswords() {
-      return this.password == this.confirmPassword ? 'Mot de passe incorect' : ''
-      }
 
-    },
     methods: {
       submit () {
         if (this.$refs.form.validate()) {
           // Native form submission is not yet supported
           axios.post('/api/submit', {
             name: this.name,
-            email: this.email
+            email: this.email,
+            password: this.password,
+            confirmPassword: this.confirmPassword
+
+
           })
         }
       }
-    }
+    },
+    computed: {
+      comparePasswords() {
+        return this.Password !== this.confirmPassword ? "Mot de passe incorect" : ""
+       }
+    },
+
   }
 </script>
 
