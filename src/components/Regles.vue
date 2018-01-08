b3757860fd77839f483b610b0121e453922c9fa2
ludovic-mrmt <ludovic.mrmt@eduge.ch>
2018-01-08 16:12:39 +0100

    test

--- src/components/Regles.vue
+++ src/components/Regles.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="hello">
-    <p>The rules page</p>
+    <template>
+       <div class = "hello">
+       <img src= "../assets/Uttt3_1.png"  height="600" align="center">
+       <H1>    ...</H1>
+       <img src= "../assets/Uttt3_2.png"  height="600" align="left">
+       </div>
+    </template>
   </div>
 </template>
